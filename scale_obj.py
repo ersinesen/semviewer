ab3de91ad2a9b2b2bd478189e023805e0991c4b0
Ersin Esen <admin@example.com>
2021-01-16 02:03:45 +0300

    mesh centered

--- scale_obj.py
+++ scale_obj.py
@@ -5,15 +5,31 @@
 
 SCALE = 10000.0
 
+
 with open('36158833.obj', 'r') as f:
 	lines = f.read().splitlines()
-	with open('my.obj', 'w') as f2:
-		for line in lines:
-			if line[0] == 'v':
-				parts = line.split()
-				x = float(parts[1]) / SCALE
-				y = float(parts[2]) / SCALE
-				z = float(parts[3]) / SCALE
-				f2.write('v {} {} {}\n'.format(x,y,z))
-			else:
-				f2.write('{}\n'.format(line))
+
+# find center of mass
+X = Y = Z = []
+for line in lines:
+	if line[0] == 'v':
+		parts = line.split()
+		X.append(float(parts[1]))
+		Y.append(float(parts[2]))
+		Z.append(float(parts[3]))
+
+mx = sum(X) / len(X)
+my = sum(Y) / len(Y)
+mz = sum(Z) / len(Z)
+
+# write centered and scaled coordinates
+with open('my.obj', 'w') as f2:
+	for line in lines:
+		if line[0] == 'v':
+			parts = line.split()
+			x = (float(parts[1]) - mx) / SCALE
+			y = (float(parts[2]) - my) / SCALE
+			z = (float(parts[3]) - mz) / SCALE
+			f2.write('v {:.2f} {:.2f} {:.2f}\n'.format(x,y,z))
+		else:
+			f2.write('{}\n'.format(line))
